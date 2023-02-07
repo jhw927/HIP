4e5970beab0e3b67fd69960415c8263c615f9301
JoHyunwoo <jhw3874@gmail.com>
2023-02-08 01:44:01 +0900

    #1 - .gitignore

---
+++ HIP/src/test/java/borad/HIP/HipApplicationTests.java
@@ -0,0 +1,13 @@
+package borad.HIP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
